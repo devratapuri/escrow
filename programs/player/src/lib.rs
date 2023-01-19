88829ecb95d62c3cf9a8b4debafcc337270347e4
Devrata puri <68563387+devratapuri@users.noreply.github.com>
2023-01-19 22:42:26 +0530

    Update

--- programs/player/src/lib.rs
+++ programs/player/src/lib.rs
@@ -1,30 +1,74 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
-declare_id!("8H1uF9UVq3y8FPimZAjXA9NDLf2jPUwr7eDqrg97WWPG");
+declare_id!("6riKjKqR6aoqevBgaqaU9Kzu85jjoFKPBPAVP3ZKukyz");
 
 #[program]
 pub mod game {
     use super::*;
     // handler function
-    pub fn create_user_stats(ctx: Context<CreateUserStats>, name: String,amount: u32,action:bool) -> Result<()> {
+    pub fn create_user_stats(ctx: Context<CreateUserStats>, name: String) -> Result<()> {
         let user_stats = &mut ctx.accounts.user_stats;
         user_stats.level = 0;
         if name.as_bytes().len() > 200 {
             // proper error handling omitted for brevity
             panic!();
         }
-        if action == true {
-            user_stats.deposit.push(amount);
-            user_stats.balance+=amount;
-        }else {
-            user_stats.deposit.push(amount);
-            user_stats.balance-=amount;
-        }
+        // if action == true {
+        //     user_stats.deposit.push(amount);
+        //     user_stats.balance+=amount;
+        // }else {
+        //     user_stats.deposit.push(amount);
+        //     user_stats.balance-=amount;
+        // }
         user_stats.player_name = name;
-        user_stats.bumps = *ctx.bumps.get("user_balance").unwrap();
-        user_stats.action = action;
+        user_stats.deposit = vec![0];
+        user_stats.withdrawl = vec![0];
+        user_stats.balance = 0;
         Ok(())
     }
+
+    pub fn deposit(ctx: Context<Update>, amount: u32) -> Result<()> {
+        let user_stats = &mut ctx.accounts.user_stats;
+        user_stats.deposit.push(amount);
+        msg!("deposit");
+        user_stats.balance += amount;
+        Ok(())
+    }
+    pub fn withdrawl(ctx: Context<Update>, amount: u32) -> Result<()> {
+        let user_stats = &mut ctx.accounts.user_stats;
+        user_stats.deposit.push(amount);
+        msg!("withdrawl");
+        user_stats.balance -= amount;
+        Ok(())
+    }
+
+}
+
+// validation struct
+#[derive(Accounts)]
+pub struct CreateUserStats<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        space = size_of::<UserStats>() + 16, seeds = ["user-stats".as_bytes(), user.key().as_ref()], bump
+    )]
+    pub user_stats: Account<'info, UserStats>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+            mut,
+        seeds = ["user-stats".as_bytes(), user.key().as_ref()], 
+        bump = user_stats.bump,
+    )]
+    pub user_stats: Account<'info, UserStats>,
 }
 
 #[account]
@@ -34,21 +78,5 @@ pub struct UserStats {
     deposit: Vec<u32>,
     withdrawl: Vec<u32>,
     balance: u32,
-    action:bool,
-    bumps: u8
-}
-
-// validation struct
-#[derive(Accounts)]
-pub struct CreateUserStats<'info> {
-    #[account(mut)]
-    pub user: Signer<'info>,
-    // space: 8 discriminator + 2 level + 4 name length + 200 name + 1 bump
-    #[account(
-        init,
-        payer = user,
-        space = 8 + 2 + 4 + 200 +4 + 4+ 4+4+4+1  + 1, seeds = [b"user-stats", user.key().as_ref()], bump
-    )]
-    pub user_stats: Account<'info, UserStats>,
-    pub system_program: Program<'info, System>,
+    bump: u8,
 }
